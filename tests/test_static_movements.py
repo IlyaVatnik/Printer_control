ace13dc1da78a7d76475146a35c2ee52ff9c3800
IlyaVatnik <ilya.vatnik@gmail.com>
2026-02-09 14:07:40 +0700

    Update test_static_movements.py

--- tests/test_static_movements.py
+++ tests/test_static_movements.py
@@ -23,12 +23,17 @@ print(p.printer_info())
 
 
 p.home('XYZ')
-p.set_motion_limits(velocity_mm_s=100, accel_mm_s2=500)
+velocity_mm_s=100
+accel_mm_s2=500
+p.set_motion_limits(velocity_mm_s=velocity_mm_s, accel_mm_s2=accel_mm_s2)
 #%%
 z_safe=100
 z_contact=96
 
 
-for x in np.arange(100,150,2):
-    for y in np.arange(150,180,2):
-        p.safe_y_pass(x=x, y_start=y, y_end=y, z_safe=z_safe, z_contact=z_contact)
+for x in np.arange(300,380,10):
+    for y in np.arange(300,400,10):
+        p.safe_y_pass(x=x, y_start=y, y_end=y, z_safe=z_safe, z_contact=z_safe)
+        p.move_absolute(x=x, y=y, z=z_contact, speed_mm_s=velocity_mm_s)
+        p.move_absolute(x=x, y=y, z=z_safe, speed_mm_s=velocity_mm_s)
+        
